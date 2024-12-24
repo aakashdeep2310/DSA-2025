fdaf483415412d173559560affe6f0c0e17e0bd1
Aakash Deep <90546941+aakashdeep2310@users.noreply.github.com>
2024-12-24 09:18:55 +0530

    Learning BitonicArray in Binary Seach Playlist

---
+++ Binary Search/BitonicArray.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean validMountainArray(int[] arr) {
+
+        int n = arr.length;
+        int i = 0;
+        while(i<n-1){
+            if(arr[i] < arr[i+1]){
+                i++;
+            }else{
+                break;
+            }
+        }
+
+        if(i == 0 || i == n-1){
+            return false;
+        }
+
+        while(i<n-1){
+            if(arr[i] > arr[i+1]){
+                i++;
+            }else{
+                break;
+            }
+        }
+        return i == n-1;
+    }
+}
