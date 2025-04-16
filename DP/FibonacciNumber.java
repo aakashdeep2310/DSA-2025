c329b6fabcd49443e744174f975762026ecfe34e
Aakash Deep <90546941+aakashdeep2310@users.noreply.github.com>
2025-04-17 00:56:43 +0530

    509. Fibonacci Number

---
+++ DP/FibonacciNumber.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int fib(int n) {
+        if(n<=1) return n;
+
+        int dp[] = new int[n+1];
+
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for(int state = 2; state<=n; state++){
+            dp[state] = dp[state-1] + dp[state-2]; 
+        }
+
+        return dp[n];
+    }
+}
