70fd13b327005862b841986e869bd15f21a648d2
Aakash Deep <90546941+aakashdeep2310@users.noreply.github.com>
2025-04-17 01:02:29 +0530

    Update FibonacciNumber.java

--- DP/FibonacciNumber.java
+++ DP/FibonacciNumber.java
@@ -2,15 +2,27 @@ class Solution {
     public int fib(int n) {
         if(n<=1) return n;
 
-        int dp[] = new int[n+1];
+        // int dp[] = new int[n+1];
 
-        dp[0] = 0;
-        dp[1] = 1;
+        // dp[0] = 0;
+        // dp[1] = 1;
 
-        for(int state = 2; state<=n; state++){
-            dp[state] = dp[state-1] + dp[state-2]; 
+        // for(int state = 2; state<=n; state++){
+        //     dp[state] = dp[state-1] + dp[state-2]; 
+        // }
+
+        // return dp[n];
+
+        // approach to save memory
+        int prev2 = 0;
+        int prev1 = 1;
+        int ans = 0;
+
+        for(int state = 2; state <= n; state++){
+            ans = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = ans;
         }
-
-        return dp[n];
+        return ans;
     }
 }
